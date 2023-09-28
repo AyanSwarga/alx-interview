526064c0ccdb7cd7243e959eda7c580cac4404fa
AyanSwarga <ayannicholas@gmail.com>
2023-09-28 20:23:46 +0100

    task

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""This Function generates pascal triangle"""
+
+
+def pascal_triangle(n):
+    """Pascal triangle function"""
+    our_list = []
+    for i in range(n):
+        mini_list = []
+        for j in range(i + 1):
+            mini_list.append(factorial(i)//(factorial(j)*factorial(i-j)))
+        our_list.append(mini_list)
+    return our_list
+
+
+def factorial(n):
+    """Function to calculate factorial"""
+    return 1 if (n == 1 or n == 0) else n * factorial(n - 1)
