fdd36db44a1bd222b17626d6f718653943d89d40
AyanSwarga <ayannicholas@gmail.com>
2023-10-18 15:05:02 +0100

    update

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,87 +1,60 @@
 #!/usr/bin/python3
-'''A script for parsing HTTP request logs.
-'''
-import re
+"""log parsing module."""
 
 
-def extract_input(input_line):
-    '''Extracts sections of a line of an HTTP request log.
-    '''
-    fp = (
-        r'\s*(?P<ip>\S+)\s*',
-        r'\s*\[(?P<date>\d+\-\d+\-\d+ \d+:\d+:\d+\.\d+)\]',
-        r'\s*"(?P<request>[^"]*)"\s*',
-        r'\s*(?P<status_code>\S+)',
-        r'\s*(?P<file_size>\d+)'
-    )
-    info = {
-        'status_code': 0,
-        'file_size': 0,
-    }
-    log_fmt = '{}\\-{}{}{}{}\\s*'.format(fp[0], fp[1], fp[2], fp[3], fp[4])
-    resp_match = re.fullmatch(log_fmt, input_line)
-    if resp_match is not None:
-        status_code = resp_match.group('status_code')
-        file_size = int(resp_match.group('file_size'))
-        info['status_code'] = status_code
-        info['file_size'] = file_size
-    return info
+def processLine(line: str, status_codes: dict) -> int:
+    """
+    The line processing function.
+    Extracts the status code and the file size
+    from the line.
+    """
+    if 'GET /projects' in line:
+        try:
+            fsize = int(line.split()[-1])
+            status = int(line.split()[-2])
+            if status in status_codes.keys():
+                status_codes[status] += 1
+            return fsize
+        except ValueError:
+            pass
+    return 0
 
 
-def print_statistics(total_file_size, status_codes_stats):
-    '''Prints the accumulated statistics of the HTTP request log.
-    '''
-    print('File size: {:d}'.format(total_file_size), flush=True)
-    for status_code in sorted(status_codes_stats.keys()):
-        num = status_codes_stats.get(status_code, 0)
+def logLines(total_file_size: int, status_codes: dict):
+    """Logs the processed lines to the standard output."""
+    print(f'File size: {total_file_size}', flush=True)
+    for code in sorted(status_codes):
+        num = status_codes.get(code, 0)
         if num > 0:
-            print('{:s}: {:d}'.format(status_code, num), flush=True)
-
-
-def update_metrics(line, total_file_size, status_codes_stats):
-    '''Updates the metrics from a given HTTP request log.
-
-    Args:
-        line (str): The line of input from which to retrieve the metrics.
-
-    Returns:
-        int: The new total file size.
-    '''
-    line_info = extract_input(line)
-    status_code = line_info.get('status_code', '0')
-    if status_code in status_codes_stats.keys():
-        status_codes_stats[status_code] += 1
-    return total_file_size + line_info['file_size']
+            print(f'{code}: {num}', flush=True)
 
 
 def run():
-    '''Starts the log parser.
-    '''
-    line_num = 0
+    """The entrance function"""
     total_file_size = 0
-    status_codes_stats = {
-        '200': 0,
-        '301': 0,
-        '400': 0,
-        '401': 0,
-        '403': 0,
-        '404': 0,
-        '405': 0,
-        '500': 0,
-    }
+    nlines = 0
+    status_codes = {
+            200: 0,
+            301: 0,
+            400: 0,
+            401: 0,
+            403: 0,
+            404: 0,
+            405: 0,
+            500: 0
+            }
     try:
         while True:
             line = input()
-            total_file_size = update_metrics(
-                line,
-                total_file_size,
-                status_codes_stats,
-            )
-            line_num += 1
-            if line_num % 10 == 0:
-                print_statistics(total_file_size, status_codes_stats)
+            total_file_size += processLine(
+                    line,
+                    status_codes
+                    )
+            nlines += 1
+            if nlines % 10 == 0:
+                logLines(total_file_size, status_codes)
     except (KeyboardInterrupt, EOFError):
-        print_statistics(total_file_size, status_codes_stats)
+        logLines(total_file_size, status_codes)
 
 
 if __name__ == '__main__':
