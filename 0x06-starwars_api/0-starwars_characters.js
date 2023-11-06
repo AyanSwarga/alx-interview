ee755cb8eb3629b6bee9103039679082c738340c
AyanSwarga <ayannicholas@gmail.com>
2023-11-06 14:34:35 +0100

    task_0

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const val = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${val}`;
+
+request.get(url,
+  (error, response, body) => {
+    if (error) console.log(error);
+    const jsonBody = JSON.parse(body);
+    const characters = jsonBody.characters;
+
+    const namePerCharacters = characters.map(
+      (url) =>
+        new Promise((resolve, reject) => {
+          request(url, (pError, pResp, pBody) => {
+            if (pError) {
+              reject(pError);
+            }
+            resolve(JSON.parse(pBody).name);
+          });
+        })
+    );
+
+    Promise.all(namePerCharacters)
+      .then((names) => {
+        for (const val in names) {
+          console.log(names[val]);
+        }
+      })
+      .catch((err) => console.log(err));
+  }
+);
